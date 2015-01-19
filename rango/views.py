330ee3a2e83f0439b7318960ce0821fadaff9367
Vian Dinh <gargrubee@gmail.com>
2015-01-19 17:05:11 +0000

    Chapter 4 - completed

---
+++ rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world! <br/> <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("ABOUT PAGE <br/> <a href='/rango'>Index</a>")
