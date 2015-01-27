2511ed34feb3d40239660e25dde2533ed252a754
2084885 <2084885d@student.glasgow.ac.uk>
2015-01-27 19:04:14 +0000

    Chapter 7 - completed

--- populate_rango.py
+++ populate_rango.py
@@ -7,47 +7,50 @@ django.setup()
 from rango.models import Category, Page
 
 
+
 def populate():
     python_cat = add_cat('Python',128,64)
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",views=3)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",views=4)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",views=5)
 
     django_cat = add_cat("Django",64,32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
-
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",views=6)
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",views=7)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",views=8)
 
     frame_cat = add_cat("Other Frameworks",32,16)
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",views=10)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",views=11)
+
     student_cat=add_cat("Vian Trieu Dinh",0,0)
-    add_page(cat=student_cat,title="Git hub account",url="https://github.com/2084885")
-    add_page(cat=student_cat,title="Python anywhere account",url="https://www.pythonanywhere.com/user/2084885/consoles/")
+
+    add_page(cat=student_cat,title="Git hub account",url="https://github.com/2084885",views=12)
+
+    add_page(cat=student_cat,title="Python anywhere account",url="https://www.pythonanywhere.com/user/2084885/consoles/",views=13)
     
     
     # Print out what we have added to the user.
@@ -55,7 +58,7 @@ def populate():
         for p in Page.objects.filter(category=c):
             print "- {0} - {1}".format(str(c), str(p))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
